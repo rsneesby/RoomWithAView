270b0037cc561dae5f0a4ac858bbf3e6050e416b
umsneesb <umsneesby@myumanitoba.ca>
2020-07-29 21:50:55 -0500

    Completed Android Room Tutorial

---
+++ app/src/main/java/com/example/roomwithaview/WordDAO.java
@@ -0,0 +1,21 @@
+package com.example.roomwithaview;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+@Dao
+public interface WordDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAlphabetizedWords();
+}
